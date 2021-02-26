b6d6e3222edd3a1258adb3c75813816db07ea538
MT-cod <mammmoth@list.ru>
2021-02-26 16:00:16 +0300

    funcs name

--- src/common_BG_functions.php
+++ src/common_BG_functions.php
@@ -3,13 +3,13 @@
 namespace Projects\lvl1;
 
 //Если переданное число чётное, то возвращаем'yes', иначе 'no'
-function even_check(int $num): string
+function checkEven(int $num): string
 {
     return ($num % 2 == 0) ? 'yes' : "no";
 }
 
 //Возвращаем случайное число от 1 до 100
-function get_rand_num(): int
+function getRandNum(): int
 {
     return random_int(1, 100);
 }
