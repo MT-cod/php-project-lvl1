faf7473547ccec3aafb65dd28701760ff824a0c9
MT-cod <mammmoth@list.ru>
2021-02-26 23:26:18 +0300

    6.15

--- src/Cli.php
+++ src/Cli.php
@@ -5,7 +5,7 @@ namespace Projects\lvl1;
 use function cli\line;
 use function cli\prompt;
 
-function run()
+function run(): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
--- src/Engine.php
+++ src/Engine.php
@@ -5,13 +5,20 @@ namespace Projects\lvl1;
 use function cli\line;
 use function cli\prompt;
 
-//Приветствуем игрока и узнаём его имя
-function promptUserNameAndGreetig(): string
+//Приветствуем игрока и узнаём его имя, запускаем нужную игру со своей логикой
+function runGame($gameName): void
 {
     line('Welcome to the Brain Game!');
     $name = prompt('May I have your name?');
     line("Hello, $name!");
-    return $name;
+    
+    switch ($operation_num) 
+    {
+        case 'brain-even':
+            line('Answer "yes" if the number is even, otherwise answer "no".');
+        case 'brain-calc':
+            line('What is the result of the expression?');
+    }
 }
 
 //Если переданное число чётное, то возвращаем'yes', иначе 'no'
@@ -26,8 +33,20 @@ function getRandNum(): int
     return random_int(1, 100);
 }
 
+//Возвращаем случайную операцию для калькулятора
+function getRandOperationForCalc(): string
+{
+    $operation_num = random_int(1, 3);
+    switch ($operation_num) 
+    {
+        case 1: return '+';
+        case 2: return '-';
+        case 3: return '/';
+    }
+}
+
 //Играем в "Проверка на чётность"
-function runBrainEvenGame()
+function runBrainEvenGame(): void
 {
     $name = promptUserNameAndGreetig();
     line('Answer "yes" if the number is even, otherwise answer "no".');
@@ -47,22 +66,8 @@ function runBrainEvenGame()
 }
 
 //Играем в "Калькулятор"
-function runCalculaterGame()
+function runCalculaterGame(): void
 {
     $name = promptUserNameAndGreetig();
     line('What is the result of the expression?');
-    for ($i = 0; $i < 3; $i++) {
-        $random_num1 = getRandNum();
-        $random_num2 = getRandNum();
-        $isEvenOrNo = checkEven($random_num);
-        line("Question: $random_num");
-        $answer = prompt('Your answer');
-        if ($answer !== $isEvenOrNo) {
-            line("'$answer' is wrong answer ;(. Correct answer was '$isEvenOrNo'.");
-            line("Let's try again, $name!");
-            exit;
-        }
-        line('Correct!');
-    }
-    line("Congratulations, $name!");
 }
